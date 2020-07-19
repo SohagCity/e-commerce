c6398dade344f2f8cadba7c74fee90306da4281d
SohagCity <Sohag.Miah@city.ac.uk>
2020-07-19 04:57:05 +0100

    deploy

---
+++ routes/api/PaymentLogs.js
@@ -0,0 +1,89 @@
+const router = require("express").Router();
+let PaymentLogs = require("../../models/PaymentLogs.model");
+
+router.route("/").get((req, res) => {
+  PaymentLogs.find()
+    .then((paymentLogs) => res.json(paymentLogs))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/add").post((req, res) => {
+  const name = req.body.name;
+  const address = req.body.address;
+  const paymentDetails = req.body.paymentDetails;
+  const products = req.body.products;
+  const total = Number(req.body.total);
+
+  const newPaymentLogs = new PaymentLogs({
+    name,
+    address,
+    paymentDetails,
+    products,
+    total,
+  });
+
+  newPaymentLogs
+    .save()
+    .then(() => res.json("PaymentLogs added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").get((req, res) => {
+  PaymentLogs.findById(req.params.id)
+    .then((paymentLogs) => res.json(paymentLogs))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").delete((req, res) => {
+  PaymentLogs.findByIdAndDelete(req.params.id)
+    .then(() => res.json("PaymentLogs deleted."))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  PaymentLogs.findById(req.params.id)
+    .then((paymentLogs) => {
+      paymentLogs.name = req.body.name;
+      paymentLogs.address = req.body.address;
+      paymentLogs.card = req.body.card;
+      paymentLogs.products = req.body.products;
+      paymentLogs.total = Number(req.body.total);
+
+      paymentLogs
+        .save()
+        .then(() => res.json("PaymentLogs updated!"))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+module.exports = router;
+
+/*
+
+{
+    "name":"Tst",
+    "address":"112 sooooos",
+    "paymentDetails": {
+        "paymentMethod":"card",
+        "cardNo": 3222333456,
+        "expiryDate" : "07/5"
+    },
+    "products":[
+        {
+        "title": "Smashed Iphone",
+        "price": 2,
+        "count": 1,
+        "total": 12
+        },
+         {
+        "title": "Samsung",
+        "price": 2,
+        "count": 0,
+        "total": 0
+        }
+    ],
+    "total":12
+}
+
+*/
---
+++ routes/api/User.js
@@ -0,0 +1,178 @@
+const router = require("express").Router();
+const passport = require("passport");
+const JWT = require("jsonwebtoken");
+let User = require("../../models/User.model");
+let PaymentLogs = require("../../models/PaymentLogs.model");
+let Product = require("../../models/Product.model");
+const passportConfig = require("../../passport");
+
+require("dotenv").config();
+
+const signToken = (userID) => {
+  return JWT.sign({ iss: "Sohag", sub: userID }, process.env.secretOrKey, {
+    expiresIn: "1h",
+  });
+};
+
+router.route("/register").post((req, res) => {
+  const { username, password, role, firstName, lastName } = req.body;
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      res
+        .status(500)
+        .json({ message: { msgBody: "Error has occured", msgError: true } });
+    }
+    if (user) {
+      res.status(400).json({
+        message: { msgBody: "Username is already taken", msgError: true },
+      });
+    } else {
+      const newUser = new User({
+        username,
+        password,
+        role,
+        firstName,
+        lastName,
+      });
+      newUser
+        .save()
+        .then(() => res.json("User added!"))
+        .catch((err) => res.status(400).json("Error: " + err));
+    }
+  });
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
+  (req, res) => {
+    if (req.isAuthenticated()) {
+      const { _id, username, role } = req.user;
+      const token = signToken(_id);
+      res.cookie("access_token", token, { httpOnly: true, sameSite: true });
+      res.status(200).json({ isAuthenticated: true, user: { username, role } });
+    }
+  }
+);
+router.get(
+  "/logout",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.clearCookie("access_token");
+    res.json({ user: { username: "", role: "" }, success: true });
+  }
+);
+
+router.get(
+  "/paymentLogs",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    User.findById({ _id: req.user._id })
+      .populate("paymentLogs")
+      .exec((err, document) => {
+        if (err)
+          res.status(500).json({
+            message: { msgBody: "Error has occured", msgError: true },
+          });
+        else {
+          res
+            .status(200)
+            .json({ paymentLogs: document.paymentLogs, authenticated: true });
+        }
+      });
+  }
+);
+
+router.post(
+  "/paymentLogs/add",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const address = req.body.address;
+    const cardName = req.body.card.name;
+    const number = Number(req.body.card.number);
+    const expiry = req.body.card.expiry;
+    const cvv = Number(req.body.card.cvv);
+
+    const total = Number(req.body.total);
+
+    const paymentLog = new PaymentLogs({
+      firstName,
+      lastName,
+      address,
+      card: {
+        name: cardName,
+        number: number,
+        expiry: expiry,
+        cvv: cvv,
+      },
+      total,
+      products: req.body.products,
+    });
+
+    paymentLog
+      .save()
+      .then(() => {
+        req.user.paymentLogs.push(paymentLog);
+        req.user
+          .save()
+          .then(() => res.json("Successfully created paymentLog"))
+          .catch((err) => res.status(400).json("Error: " + err));
+      })
+      .catch((err) => res.status(400).json("Error: " + err));
+  }
+);
+/*
+router.post(
+  "/paymentLogs/add",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const paymentLog = new PaymentLogs(req.body);
+    paymentLog.save((err) => {
+      if (err)
+        res
+          .status(500)
+          .json({ message: { msgBody: "Error has occured", msgError: true } });
+      else {
+        req.user.paymentLogs.push(paymentLog);
+        req.user.save((err) => {
+          if (err)
+            res.status(500).json({
+              message: { msgBody: "Error has occured", msgError: true },
+            });
+          else
+            res.status(200).json({
+              message: {
+                msgBody: "Successfully created paymentLog",
+                msgError: false,
+              },
+            });
+        });
+      }
+    });
+  }
+);
+*/
+router.get(
+  "/admin",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    if (req.user.role === "admin") {
+      res.status(200).json("you are an admin");
+    } else {
+      res.status(403).json("you are not an admin");
+    }
+  }
+);
+
+router.get(
+  "/auth",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { username, role } = req.user;
+    res.status(200).json({ isAuthenticated: true, user: { username, role } });
+  }
+);
+
+module.exports = router;
