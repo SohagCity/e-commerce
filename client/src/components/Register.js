ad11fe525279803075524d22443b9ea50a1a7260
SohagCity <Sohag.Miah@city.ac.uk>
2020-07-19 03:24:45 +0100

    new register with first and last name

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,12 +1,38 @@
 import React, { Component } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Col } from "react-bootstrap";
 import AuthService from "../Services/AuthService";
 import { Link } from "react-router-dom";
 
 class Register extends Component {
   state = {
-    user: { username: "", password: "", role: "user" },
-    message: "",
+    user: {
+      firstName: "",
+      lastName: "",
+      username: "",
+      password: "",
+      role: "user",
+    },
+    message: null,
+    validated: false,
+  };
+  onChangeFirstName = (e) => {
+    e.preventDefault();
+    const user = this.state.user;
+    user.firstName = e.target.value;
+    this.setState({
+      user,
+    });
+    console.log(this.state.user);
+  };
+
+  onChangeLastName = (e) => {
+    e.preventDefault();
+    const user = this.state.user;
+    user.lastName = e.target.value;
+    this.setState({
+      user,
+    });
+    console.log(this.state.user);
   };
 
   onChangeUserName = (e) => {
@@ -30,22 +56,27 @@ class Register extends Component {
     const user = this.state.user;
     user.username = "";
     user.password = "";
-    //user.role = "";
     this.setState({ user });
   };
 
   onSubmit = (e) => {
     e.preventDefault();
-    AuthService.register(this.state.user).then((data) => {
-      const { message } = data;
-      this.setState({ message });
-      if (message) {
-        this.resetForm();
-      } else {
-        this.props.history.push("/login");
-      }
-      //
-    });
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    } else {
+      AuthService.register(this.state.user).then((data) => {
+        const { message } = data;
+        this.setState({ message });
+        if (message) {
+          this.resetForm();
+        } else {
+          this.props.history.push("/login");
+        }
+      });
+    }
+    this.setState({ validated: true });
   };
   render() {
     return (
@@ -61,35 +92,56 @@ class Register extends Component {
             There has been an error, try again
           </h3>
         ) : null}
-        <Form onSubmit={this.onSubmit}>
+        <Form
+          noValidate
+          validated={this.state.validated}
+          onSubmit={this.onSubmit}
+        >
+          <Form.Row>
+            <Form.Group as={Col}>
+              <Form.Label>First Name</Form.Label>
+              <Form.Control
+                required
+                name="firstName"
+                onChange={this.onChangeFirstName}
+                placeholder="Enter First Name"
+              />
+            </Form.Group>
+
+            <Form.Group as={Col}>
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control
+                required
+                name="lastName"
+                onChange={this.onChangeLastName}
+                placeholder="Enter Last Name"
+              />
+            </Form.Group>
+          </Form.Row>
+
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
             <Form.Control
+              required
+              name="username"
               onChange={this.onChangeUserName}
               type="email"
-              value={this.state.user.username}
               placeholder="Enter email"
             />
-            {/*<Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-    </Form.Text> */}
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control
+              name="password"
               onChange={this.onChangePassword}
               type="password"
-              value={this.state.user.password}
               placeholder="Password"
             />
           </Form.Group>
           <Form.Text className="text-muted">
             Already a member? <Link to="/login"> Sign In.</Link>
           </Form.Text>
-          {/*<Form.Group controlId="formBasicCheckbox">
-            <Form.Check type="checkbox" label="Check me out" />
-</Form.Group>*/}
           <div className="text-center">
             <Button variant="primary" type="submit">
               Register
